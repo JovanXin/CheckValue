9b002fe469964700beb7b664240b08ec37fd063c
Jovan Xin <61258436+JovanXin@users.noreply.github.com>
2020-03-22 11:10:20 +1300

    Add line breaks
    +line breaks
    +Clarify user input

--- checkvalue.py
+++ checkvalue.py
@@ -6,14 +6,17 @@ This function prompts user to enter either a valid string or a number. If choice
 
 def checkvalue(*choice): #ensures user types a str or int
   while True: 
-    value = input("enter a value:")
+    if choice:
+      value = input("enter a number:")
+    else:
+      value = input("enter a string:")
     try:
       value = int(value) #tries to convert "value" to a integer
       if choice: 
         return value
-      print("not a string, try again:") #we don't need else here because returning the value breaks the loop anways
+      print("not a string, try again \n") #we don't need else here because returning the value breaks the loop anways
     except ValueError:
       if choice: 
-        print("not a number, try again:")
+        print("not a number, try again \n")
       else:
         return value
